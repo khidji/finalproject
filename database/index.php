8ed911c6821896893e75314ad4a1ff7f49c11306
khidji <dupirelucie@gmail.com>
2021-07-21 10:48:44 +0200

    test bdd

--- database/index.php
+++ database/index.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/env.php';
 
 try {
-  $pdo = new PDO((mysql:dbname=finalproject;host=localhost), $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+  $pdo = new PDO('mysql:host=localhost;dbname=finalproject', $_ENV['DB_USER'], $_ENV['DB_PASS'], [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   ]);
